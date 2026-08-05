acd83920efb0e91a3bfe78646f3cfc7c9d999175
aleksejpaholkin8-eng <aleksejpaholkin8@gmail.com>
2026-08-05 22:54:57 +0900

    Update states.py

--- bot/states.py
+++ bot/states.py
@@ -2,33 +2,27 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class UserRegistration(StatesGroup):
-    """Диалог регистрации"""
     waiting_for_name = State()
     waiting_for_goal = State()
     waiting_for_confirm = State()
 
 
 class ByokInput(StatesGroup):
-    """Ввод своего API-ключа"""
     waiting_for_key = State()
 
 
 class UploadPrompt(StatesGroup):
-    """Диалог загрузки промпта"""
     waiting_for_file = State()
 
 
 class AdminEditLimit(StatesGroup):
-    """Изменение лимита в админ-панели"""
     waiting_for_value = State()
 
 
 class TrackMenu(StatesGroup):
-    """Диалог управления треками через кнопки"""
-    waiting_for_name = State()      
-    action = State()                 
+    waiting_for_name = State()
+    action = State()
 
 
 class FocusInput(StatesGroup):
-    """Диалог установки фокуса через кнопку"""
     waiting_for_topic = State()
